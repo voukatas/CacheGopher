27d57269f4c07d084dd3ed0f2b8b29c0a61c2f61
voukatas <sttelioss@gmail.com>
2024-06-08 19:21:59 +0300

    added cli cmd code

--- cmd/cachegopher-cli/main.go
+++ cmd/cachegopher-cli/main.go
@@ -1,4 +1,61 @@
 package main
 
-// ToDo: Placeholder to implement the command cli administrator tool
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
 
+func main() {
+
+	ip := flag.String("host", "", "Set the ip of the host")
+	port := flag.String("port", "", "Set the port of the host")
+
+	flag.Parse()
+
+	address := fmt.Sprintf("%s:%s", *ip, *port)
+
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Fatal("Failed to connect: ", err.Error())
+	}
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	respScanner := bufio.NewScanner(conn)
+	//fmt.Print(">> ")
+
+	for {
+
+		fmt.Print(">> ")
+		scanner.Scan()
+		//fmt.Println("You wrote: ", scanner.Text())
+
+		if strings.Contains(strings.ToUpper(scanner.Text()), "EXIT") {
+			fmt.Println("Bye!!")
+			return
+		}
+		//fmt.Print(">> ")
+		//fmt.Println("I will send: ", scanner.Text())
+
+		_, err := conn.Write([]byte(scanner.Text() + "\n"))
+		if err != nil {
+			fmt.Println("Write error:", err)
+			continue
+		}
+
+		//fmt.Println("Waiting for response...")
+
+		if respScanner.Scan() {
+			fmt.Println(respScanner.Text())
+		}
+		if err := respScanner.Err(); err != nil {
+			fmt.Println(err.Error())
+		}
+
+	}
+}
